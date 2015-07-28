46bac6a7d80820661baf4439de985b34269b9aba
Dean Dierschow <ddierschow@xocolatl.com>
2015-07-28 04:17:02 +0200

    Made new images using tighter storage function.

--- flags.php
+++ flags.php
@@ -19,7 +19,7 @@ The original intention of these flags were for use as in-line graphics with text
 These flags have been designed to be of a correct aspect ratio,
 most with a height of 12 to 14 pixels, but some as little as 9 or as many as 16.
 The widths mostly range from 13 to 28 pixels, with the smallest being 7 and the largest 33.
-The sizes are less than a thousand <u>bytes</u>, and most are under 300!
+The sizes are less than 300 <u>bytes</u>!
 They have also been designed with web-safe colors.
 The image files have been named with the ISO3166 country codes for entities that have these codes assigned.
 <p>
